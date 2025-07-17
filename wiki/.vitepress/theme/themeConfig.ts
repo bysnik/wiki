0a3a57f8619894b3177d3ee493b2e4e37199ce81
BystrovNO <bystrovno@basealt.ru>
2025-07-17 13:45:27 +0300

    Switch On Search engine

--- wiki/.vitepress/theme/themeConfig.ts
+++ wiki/.vitepress/theme/themeConfig.ts
@@ -45,34 +45,34 @@ export const themeConfig: DefaultTheme.Config = {
     },
 
 
-//     search: {
-//       provider: 'local',
-//       options: {
-//         locales: {
-//           ru: { // используйте ключ `root`, если хотите перевести локаль по умолчанию
-//             translations: {
-//               button: {
-//                 buttonText: 'Поиск',
-//                 buttonAriaLabel: 'Поиск'
-//               },
-//               modal: {
-//                 displayDetails: 'Отобразить подробный список',
-//                 resetButtonTitle: 'Сбросить поиск',
-//                 backButtonTitle: 'Закрыть поиск',
-//                 noResultsText: 'Нет результатов по запросу',
-//                 footer: {
-//                   selectText: 'выбрать',
-//                   selectKeyAriaLabel: 'выбрать',
-//                   navigateText: 'перейти',
-//                   navigateUpKeyAriaLabel: 'стрелка вверх',
-//                   navigateDownKeyAriaLabel: 'стрелка вниз',
-//                   closeText: 'закрыть',
-//                   closeKeyAriaLabel: 'esc'
-//                 }
-//               },
-//             }
-//           }
-//         }
-//       }
-//     }
+    search: {
+      provider: 'local',
+      options: {
+        locales: {
+          ru: { // используйте ключ `root`, если хотите перевести локаль по умолчанию
+            translations: {
+              button: {
+                buttonText: 'Поиск',
+                buttonAriaLabel: 'Поиск'
+              },
+              modal: {
+                displayDetails: 'Отобразить подробный список',
+                resetButtonTitle: 'Сбросить поиск',
+                backButtonTitle: 'Закрыть поиск',
+                noResultsText: 'Нет результатов по запросу',
+                footer: {
+                  selectText: 'выбрать',
+                  selectKeyAriaLabel: 'выбрать',
+                  navigateText: 'перейти',
+                  navigateUpKeyAriaLabel: 'стрелка вверх',
+                  navigateDownKeyAriaLabel: 'стрелка вниз',
+                  closeText: 'закрыть',
+                  closeKeyAriaLabel: 'esc'
+                }
+              },
+            }
+          }
+        }
+      }
+    }
   }
