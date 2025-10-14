979f01f917d6d63670966b438fac53eab7a8a1b8
Nikita Bystrov <bystrovno@basealt.ru>
2025-10-14 13:13:48 +0300

    start znuny 7.2.3

--- wiki/.vitepress/theme/sidebarConfig.ts
+++ wiki/.vitepress/theme/sidebarConfig.ts
@@ -306,7 +306,6 @@ export const sidebarConfig: DefaultTheme.Sidebar = {
         { text: 'MTS Link', link: '/docs/wi/mts-link' },
         { text: '! n8n', link: '/docs/wi/n8n' },
         { text: '! OpenStack', link: '/docs/wi/openstack' }, 
-        { text: '0 Oracle-DBD', link: '/docs/wi/oracle-dbd' }, 
         { text: '! RustDesk', link: '/docs/wi/rustdesk' },
         { text: 'Uptime Kuma', link: '/docs/wi/uptime-kuma' },
       ]
@@ -485,7 +484,15 @@ export const sidebarConfig: DefaultTheme.Sidebar = {
         { text: '0 Zabbix', link: '/docs/wi/zabbix' },
       ]
     },
-
+      
+    {
+      text: 'Сервисное ПО',
+      collapsed: true,
+      items: [
+        { text: '0 Oracle-DBD', link: '/docs/wi/oracle-dbd' },
+      ]
+    },
+    
     {
       text: 'Сетевое туннелирование',
       collapsed: true,
